698dbd0bb3c1a6172531de14fae88fd202cd7dad
Eugene Polunin <altahoma@gmail.com>
2023-08-09 17:05:41 +0300

    Added str representation for models

--- station/models.py
+++ station/models.py
@@ -7,19 +7,28 @@ class Station(models.Model):
     latitude = models.FloatField()
     longitude = models.FloatField()
 
+    def __str__(self):
+        return self.name
+
 
 class Route(models.Model):
     source = models.ForeignKey(
-        to=Station, on_delete=models.CASCADE, realted_name="routes"
+        to=Station, on_delete=models.CASCADE, related_name="routes_source"
     )
     destination = models.ForeignKey(
-        to=Station, on_delete=models.CASCADE, realted_name="routes"
+        to=Station, on_delete=models.CASCADE, related_name="routes_destination"
     )
     distance = models.IntegerField()
+
+    def __str__(self):
+        return f"{self.source} to {self.destination}"
 
 
 class TrainType(models.Model):
     name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
 
 
 class Train(models.Model):
@@ -27,40 +36,55 @@ class Train(models.Model):
     cargo_num = models.IntegerField()
     places_in_cargo = models.IntegerField()
     train_type = models.ForeignKey(
-        to=TrainType, on_delete=models.CASCADE, realted_name="trains"
+        to=TrainType, on_delete=models.CASCADE, related_name="trains"
     )
+
+    def __str__(self):
+        return self.name
 
 
 class Crew(models.Model):
     first_name = models.CharField(max_length=255)
     last_name = models.CharField(max_length=255)
 
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+
 
 class Journey(models.Model):
-    crews = models.ManyToManyField(to=Crew, realted_name="journeys")
+    crews = models.ManyToManyField(to=Crew, related_name="journeys")
     route = models.ForeignKey(
-        to=Route, on_delete=models.CASCADE, realted_name="journeys"
+        to=Route, on_delete=models.CASCADE, related_name="journeys"
     )
     train = models.ForeignKey(
-        to=Train, on_delete=models.CASCADE, realted_name="journeys"
+        to=Train, on_delete=models.CASCADE, related_name="journeys"
     )
     departure_time = models.DateTimeField()
     arrival_time = models.DateTimeField()
+
+    def __str__(self):
+        return f"{self.route} by {self.train}"
 
 
 class Order(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     user = models.ForeignKey(
-        to=get_user_model(), on_delete=models.CASCADE, realted_name="orders"
+        to=get_user_model(), on_delete=models.CASCADE, related_name="orders"
     )
+
+    def __str__(self):
+        return f"N.{self.id}"
 
 
 class Ticket(models.Model):
     cargo = models.IntegerField()
     seat = models.IntegerField()
     journey = models.ForeignKey(
-        to=Journey, on_delete=models.CASCADE, realted_name="tickets"
+        to=Journey, on_delete=models.CASCADE, related_name="tickets"
     )
     order = models.ForeignKey(
-        to=Order, on_delete=models.CASCADE, realted_name="tickets"
+        to=Order, on_delete=models.CASCADE, related_name="tickets"
     )
+
+    def __str__(self):
+        return f"Seat: {self.seat} to {self.journey}"
