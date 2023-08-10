b7d0e4b867940b96f12a477581f8b92b44705abb
Eugene Polunin <altahoma@gmail.com>
2023-08-10 23:58:44 +0300

    Implemented swagger

--- config/urls.py
+++ config/urls.py
@@ -1,14 +1,19 @@
 from django.contrib import admin
 from django.urls import path, include
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/station/", include("station.urls", namespace="station")),
     path("__debug__/", include("debug_toolbar.urls")),
+    path("api/doc/", SpectacularAPIView.as_view(), name="schema"),
+    path(
+        "api/doc/swagger/",
+        SpectacularSwaggerView.as_view(url_name="schema"),
+        name="swagger-ui",
+    ),
 ]
 
-# todo: add swagger
-# todo: pagination
 # todo: user and auth
 # todo: add JWT
 # todo: add tests
